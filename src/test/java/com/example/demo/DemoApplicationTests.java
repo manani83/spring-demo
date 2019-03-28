649bd7ccee9a940dfeacf1363d6b8f451bfac94b
mhkimaa <mhkimaa@pulmuone.com>
2019-03-28 19:56:50 +0900

    second commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,41 @@
 package com.example.demo;
 
+import com.example.demo.domain.posts.Posts;
+import com.example.demo.domain.posts.PostsRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+	@Autowired
+	PostsRepository postsRepository;
+
 
 	@Test
-	public void contextLoads() {
+	public void BaseTimeEntity_등록 () {
+		//given
+		LocalDateTime now = LocalDateTime.now();
+		postsRepository.save(Posts.builder()
+				.title("테스트 게시글")
+				.content("테스트 본문")
+				.author("jojoldu@gmail.com")
+				.build());
+		//when
+		List<Posts> postsList = postsRepository.findAll();
+
+		//then
+		Posts posts = postsList.get(0);
+		assertTrue(posts.getCreatedDate().isAfter(now));
+		assertTrue(posts.getModifiedDate().isAfter(now));
 	}
 
 }
